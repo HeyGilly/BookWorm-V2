462db791ef3d7e7e03936a61786b1f0a5c818ba8
heygilly <nick.gilmore20@gmail.com>
2023-11-06 00:40:47 -0600

    Initial Commit

---
+++ src/main/java/com/bookworm/bookwormv2/BookwormV2Application.java
@@ -0,0 +1,13 @@
+package com.bookworm.bookwormv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwormV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwormV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookworm/bookwormv2/BookwormV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookworm.bookwormv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwormV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
