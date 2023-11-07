2c9d8faee7cd57ab344622bd2a897e97be2bf5da
heygilly <nick.gilmore20@gmail.com>
2023-11-07 14:02:40 -0600

    Creating a class for a user object

---
+++ src/main/java/com/bookworm/bookwormv2/models/user.java
@@ -0,0 +1,10 @@
+package com.bookworm.bookwormv2.models;
+
+public class user {
+
+    private long id;
+    private String first_name;
+    private String last_name;
+    private String username;
+
+}
