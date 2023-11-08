7c1f1471550a43485e78793771251304446477a8
heygilly <nick.gilmore20@gmail.com>
2023-11-07 20:34:02 -0600

    Converted the class to an entity

--- src/main/java/com/bookworm/bookwormv2/models/User.java
+++ src/main/java/com/bookworm/bookwormv2/models/User.java
@@ -1,21 +1,41 @@
 package com.bookworm.bookwormv2.models;
 
-public class user {
 
+import jakarta.persistence.*;
+
+import java.math.BigInteger;
+
+//-- Entity refers to a Java class that represents an object or record in your database.
+//-- Serves as a bridge between your Java application and the database.
+//-- The annotation Entity tells spring data JPA that this class is an entity and should be mapped ot the database table.
+@Entity
+//-- Used to specify the name of the database table that corresponds to your entity class.
+//-- Used to customize the table name if it should be different from the default naming convention.
+@Table(name = "USER")
+public class User {
+    @Id
+    @Column(name="USER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name="FIRST_NAME")
     private String first_name;
+    @Column(name="LAST_NAME")
     private String last_name;
+    @Column(name="USERNAME")
     private String username;
+    @Column(name="EMAIL")
     private String email;
+    @Column(name="PASSWORD")
     private String password;
+    @Column(name="BIO")
     private String bio;
 
     //--Empty Constructor
-    public user() {
+    public User() {
     }
 
     //-- Whole Constructor
-    public user(long id, String first_name, String last_name, String username, String email, String password, String bio) {
+    public User(long id, String first_name, String last_name, String username, String email, String password, String bio) {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
