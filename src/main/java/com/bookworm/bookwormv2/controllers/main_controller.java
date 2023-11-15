b8b1a3b106709eab7df684996db3c3407985cd0e
heygilly <nick.gilmore20@gmail.com>
2023-11-15 10:43:59 -0600

    Bookshelf Repo was added, with new model attributes to pass book info to the page.

--- src/main/java/com/bookworm/bookwormv2/controllers/main_controller.java
+++ src/main/java/com/bookworm/bookwormv2/controllers/main_controller.java
@@ -1,14 +1,41 @@
 package com.bookworm.bookwormv2.controllers;
 
+import com.bookworm.bookwormv2.models.Bookshelf;
+import com.bookworm.bookwormv2.repository.BookshelfRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class main_controller {
+    //-- Book Repository to gather all books
+    private final BookshelfRepository bookshelfRepo;
+
+    //-- Constructor
+    public main_controller(BookshelfRepository bookshelfRepo) {
+        this.bookshelfRepo = bookshelfRepo;
+    }
+    //-- getter
+    public BookshelfRepository getBookshelfRepo() {
+        return bookshelfRepo;
+    }
+
+
 
     @GetMapping("/welcome-page")
-    public String home(){
+    public String home(Model model){
+        //-- find By Thriller Genre
+        model.addAttribute("ThrillerBooks", bookshelfRepo.findAllByGenre("thriller"));
+        model.addAttribute("FictionBooks", bookshelfRepo.findAllByGenre("fiction"));
+
+
+
         return "main/introduction-page";
     }
 
@@ -21,6 +48,7 @@ public class main_controller {
     public String userProfile(){
         return "main/userProfile";
     }
+
 
 
 }
