366dd2fd8e2a3b194f1bdc2e1433774f12ed42b2
heygilly <nick.gilmore20@gmail.com>
2023-11-15 10:42:24 -0600

    added two query methods that find all books within a genre and all genres.

--- src/main/java/com/bookworm/bookwormv2/repository/BookshelfRepository.java
+++ src/main/java/com/bookworm/bookwormv2/repository/BookshelfRepository.java
@@ -3,8 +3,18 @@ package com.bookworm.bookwormv2.repository;
 import com.bookworm.bookwormv2.models.Bookshelf;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 
 public interface BookshelfRepository extends JpaRepository<Bookshelf, Long> {
+
+    //== Find By genre
+    List<Bookshelf> findAllByGenre(String name);
+    List<Bookshelf> findAllGenre();
+
+
+
 
 }
