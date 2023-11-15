3cec95df4dc5cedd64258d821d1a4c36c46d3ac3
heygilly <nick.gilmore20@gmail.com>
2023-11-15 11:12:17 -0600

    Create a formula that will showcase only the genre with on duplicates.

--- src/main/java/com/bookworm/bookwormv2/controllers/main_controller.java
+++ src/main/java/com/bookworm/bookwormv2/controllers/main_controller.java
@@ -34,6 +34,24 @@ public class main_controller {
         model.addAttribute("ThrillerBooks", bookshelfRepo.findAllByGenre("thriller"));
         model.addAttribute("FictionBooks", bookshelfRepo.findAllByGenre("fiction"));
 
+        //-- Retrieves all books from database, using the bookshelf Repo
+        List<Bookshelf> allBooks = bookshelfRepo.findAll();
+
+        //-- Creates an empty list to store the unique genres
+        //--  a forEach loop through each book in the allBooks list.
+        List<String> uniqueGenres = new ArrayList<>();
+        //-- Loops through the books and add unique genres to the array
+        allBooks.forEach(book -> {
+            //-- Stores all books genre inside the genre variable
+            String genre = book.getGenre().toLowerCase();
+            //-- This condition see if the empty array doesn't contain the genre
+            if (!uniqueGenres.contains(genre)) {
+                //-- the empty array does NOT contain the genre, we add it to the empty array.
+                uniqueGenres.add(genre);
+            }
+        });
+        // Pass the unique genres to the Thymeleaf template
+        model.addAttribute("AllGenres", uniqueGenres);
 
 
         return "main/introduction-page";
