e9825f558f0a98973dbe9da7681bd246cdb69090
heygilly <nick.gilmore20@gmail.com>
2023-11-27 13:37:25 -0600

    Fixed the duplicate genres that was displaying in the genres. Currently working on getting the data to display on the search of a book

--- src/main/java/com/bookworm/bookwormv2/controllers/main_controller.java
+++ src/main/java/com/bookworm/bookwormv2/controllers/main_controller.java
@@ -7,6 +7,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,23 +33,20 @@ public class main_controller {
     }
 
 
-
-    @GetMapping("/welcome-page")
-    public String home(Model model){
+    //-- This function is going to help us so we dont have to repeat what we want as in the books by genre and the list of genres.
+    private void addExistingDataToModel(Model model) {
         //-- find By Thriller Genre
         model.addAttribute("ThrillerBooks", bookshelfRepo.findAllByGenre("thriller"));
         model.addAttribute("FictionBooks", bookshelfRepo.findAllByGenre("fiction"));
-
         //-- Retrieves all books from database, using the bookshelf Repo
         List<Bookshelf> allBooks = bookshelfRepo.findAll();
-
         //-- Creates an empty list to store the unique genres
         //--  a forEach loop through each book in the allBooks list.
         List<String> uniqueGenres = new ArrayList<>();
         //-- Loops through the books and add unique genres to the array
         allBooks.forEach(book -> {
             //-- Stores all books genre inside the genre variable
-            String genre = book.getGenre().toLowerCase();
+            String genre = book.getGenre().substring(0, 1).toUpperCase() + book.getGenre().substring(1);
             //-- This condition see if the empty array doesn't contain the genre
             if (!uniqueGenres.contains(genre)) {
                 //-- the empty array does NOT contain the genre, we add it to the empty array.
@@ -51,20 +56,63 @@ public class main_controller {
         });
         // Pass the unique genres to the Thymeleaf template
         model.addAttribute("AllGenres", uniqueGenres);
+    }
 
+
+
+
+    @GetMapping("/welcome-page")
+    public String home(Model model){
+        // Add the existing data to the model
+        addExistingDataToModel(model);
+        return "main/introduction-page";
+    }
+
+    @PostMapping("/process-search")
+    public String processSearch(@ModelAttribute("searchTerm") String searchTerm, Model model) {
+        // Use the searchTerm in your logic
+        System.out.println("Received search term: " + searchTerm);
+
+        //== Turns String into an array then concats them with a + for a proper search in the url
+        String delimiter = " ";
+        String[] splitString = searchTerm.split(delimiter);
+        String conCatSearchTerm = String.join("+", splitString);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.googleapis.com/books/v1/volumes?q=" + conCatSearchTerm))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Parse the API response body as JSON
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode apiResponseJson = objectMapper.readTree(response.body());
+            // Log the parsed JSON object
+            System.out.println("Parsed JSON: " + apiResponseJson.toString());
+            // Add the parsed JSON object to the model
+            model.addAttribute("apiResponse", apiResponseJson);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Add the existing data to the model
+        addExistingDataToModel(model);
+
+
+
+        // Add the API response to the model
+        model.addAttribute("apiResponse", response.body());
 
         return "main/introduction-page";
     }
 
-    //-- Getting the action from the form with that URL
-    @PostMapping("/process-search")
-    public String processSearch(@ModelAttribute("searchTerm") String searchTerm) {
-        // Use the searchTerm in your logic
-        System.out.println("Received search term: " + searchTerm);
-
-        // Perform your search logic and return the appropriate view
-        return "redirect:/welcome-page"; // Redirect to a search results page
-    }
 
 
 
