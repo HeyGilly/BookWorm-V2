b2ae141295712504f6d94660f82dce842b02aa4f
heygilly <nick.gilmore20@gmail.com>
2023-12-02 17:53:46 -0600

    Created a repo to gather the info for a specific user

---
+++ src/main/java/com/bookworm/bookwormv2/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.bookworm.bookwormv2.repository;
+
+import com.bookworm.bookwormv2.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserById(long id);
+
+
+}
