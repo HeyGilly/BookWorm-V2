c91b7d81f40c197001dc0c9a9b845b65e96ff6fd
heygilly <nick.gilmore20@gmail.com>
2023-12-04 14:49:16 -0600

    Capture value and save to database

--- src/main/java/com/bookworm/bookwormv2/controllers/sign_up_controller.java
+++ src/main/java/com/bookworm/bookwormv2/controllers/sign_up_controller.java
@@ -1,19 +1,38 @@
 package com.bookworm.bookwormv2.controllers;
 
+import com.bookworm.bookwormv2.models.User;
+import com.bookworm.bookwormv2.repository.UserRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class sign_up_controller {
+    public final UserRepository userRepository;
+
+    public sign_up_controller(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/login")
     public String login(){
         return "SignUp/login";
     }
 
-    @GetMapping("/Sign-up")
-    public String signUp(){
+
+    @GetMapping("/sign-up")
+    public String signUp(Model model){
+        model.addAttribute("user", new User());
         return "SignUp/register";
+    }
+
+
+    @PostMapping("/sign-up")
+    public String greetingSubmit(@ModelAttribute("user") User user) {
+
+        //-- Creates a new User in Database
+        userRepository.save(user);
+        return "redirect:/login";
     }
 
 
