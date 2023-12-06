42d60ed6be20611a413db89791ce18a3a7e37e12
heygilly <nick.gilmore20@gmail.com>
2023-12-06 12:48:36 -0600

    Got Register Form to work, saving pathway to database, not image to directory

--- src/main/java/com/bookworm/bookwormv2/controllers/sign_up_controller.java
+++ src/main/java/com/bookworm/bookwormv2/controllers/sign_up_controller.java
@@ -2,23 +2,18 @@ package com.bookworm.bookwormv2.controllers;
 
 import com.bookworm.bookwormv2.models.User;
 import com.bookworm.bookwormv2.repository.UserRepository;
-import com.bookworm.bookwormv2.service.FileService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
-import java.util.UUID;
 
 @Controller
 public class sign_up_controller {
     public final UserRepository userRepository;
 
-    private final FileService fileService;
 
-    public sign_up_controller(UserRepository userRepository, FileService fileService) {
+    public sign_up_controller(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.fileService = fileService;
     }
 
     @GetMapping("/login")
@@ -35,26 +30,21 @@ public class sign_up_controller {
 
 
     @PostMapping("/sign-up")
-    public String greetingSubmit(@ModelAttribute("user")  User user, @RequestParam("profilePicturePath") MultipartFile file) {
-        try {
-            // Check if img is uploaded
-            if (!file.isEmpty()) {
-                //== Create a unique file name with filename and a random number generator
-                String fileName = "user_" + UUID.randomUUID() + "_" + file.getOriginalFilename();
-                //== Calling the File Service to run its method
-                fileService.saveFile(file, fileName);
-                // Set the file path in the User object
-                //== Will update the Database with the directory and File name
-                user.setProfilePicturePath("img/profilePicture/" + fileName);
-            }
-            // Creates a new User in the Database
-            userRepository.save(user);
-            return "redirect:/login";
-        } catch (IOException e) {
-            System.out.println("Error occurred during file processing: " + e.getMessage());
-            return "redirect:/sign-up"; // Redirect back to the login page with an error message
-        }
+    public String greetingSubmit(@ModelAttribute("user") User user, @RequestParam("profilePictureFile") MultipartFile profilePictureFile) {
+        // Handle the file and save its path to profilePicturePath
+        // For example, you can save it to a directory and store the path in the database
+        String filePath = "img/ProfilePictures/" + profilePictureFile.getOriginalFilename();
+        // Save the file to the directory
+        // ...
+
+        // Set the file path in the user object
+        user.setProfilePicturePath(filePath);
+
+        // Creates a new User in the Database
+        userRepository.save(user);
+        return "redirect:/login";
     }
+
 
 
 }
--- src/main/java/com/bookworm/bookwormv2/models/User.java
+++ src/main/java/com/bookworm/bookwormv2/models/User.java
@@ -2,8 +2,8 @@ package com.bookworm.bookwormv2.models;
 
 
 import jakarta.persistence.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.math.BigInteger;
 import java.util.List;
 
 //-- Entity refers to a Java class that represents an object or record in your database.
@@ -30,9 +30,15 @@ public class User {
     private String password;
     @Column(name="BIO")
     private String bio;
+
+    //== This is used to handle the file upload.
+    //== When Using Transient, meaning that you are usually saving the image to a file system, or a cloud storage.
+    @Transient //== Meaning the file itself won't be stored in the database
+    private MultipartFile profilePictureFile;
+
+    //== This is used to store the path associated with the profile picture.
     @Column(name="PROFILE_PICTURE_PATH")
     private String profilePicturePath;  // Store the file path or metadata here
-
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<Reviews> reviewsList;
@@ -45,9 +51,7 @@ public class User {
     }
 
     //-- Whole Constructor
-
-
-    public User(long id, String first_name, String last_name, String username, String email, String password, String bio, String profilePicturePath, List<Reviews> reviewsList, List<FavoriteGenre> favoriteGenres) {
+    public User(Long id, String first_name, String last_name, String username, String email, String password, String bio, String profilePicturePath, List<Reviews> reviewsList, List<FavoriteGenre> favoriteGenres) {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
@@ -60,43 +64,73 @@ public class User {
         this.favoriteGenres = favoriteGenres;
     }
 
-    //-- Getters and Setters
-    public long getId() {return id;}
-    public void setId(long id) {this.id = id;}
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getFirst_name() {return first_name;}
-    public void setFirst_name(String first_name) {this.first_name = first_name;}
+    public String getFirst_name() {
+        return first_name;
+    }
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
 
-    public String getLast_name() {return last_name;}
-    public void setLast_name(String last_name) {this.last_name = last_name;}
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
 
-    public String getUsername() {return username;}
-    public void setUsername(String username) {this.username = username;}
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    public String getEmail() {return email;}
+    public String getEmail() {
+        return email;
+    }
     public void setEmail(String email) {this.email = email;}
 
-    public String getPassword() {return password;}
-    public void setPassword(String password) {this.password = password;}
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-    public String getBio() {return bio;}
-    public void setBio(String bio) {this.bio = bio;}
+    public String getBio() {
+        return bio;
+    }
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 
-    public List<Reviews> getReviewsList() {return reviewsList;}
-    public void setReviewsList(List<Reviews> reviewsList) {this.reviewsList = reviewsList;}
+    public MultipartFile getProfilePictureFile() {return profilePictureFile;}
+    public void setProfilePictureFile(MultipartFile profilePictureFile) {this.profilePictureFile = profilePictureFile;}
 
     public String getProfilePicturePath() {
         return profilePicturePath;
     }
-
     public void setProfilePicturePath(String profilePicturePath) {
         this.profilePicturePath = profilePicturePath;
+    }
+
+    public List<Reviews> getReviewsList() {
+        return reviewsList;
+    }
+    public void setReviewsList(List<Reviews> reviewsList) {
+        this.reviewsList = reviewsList;
     }
 
     public List<FavoriteGenre> getFavoriteGenres() {
         return favoriteGenres;
     }
-
     public void setFavoriteGenres(List<FavoriteGenre> favoriteGenres) {
         this.favoriteGenres = favoriteGenres;
     }
