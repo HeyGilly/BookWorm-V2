accb3e4b065a670703820b853cc2e3542bb6a441
heygilly <nick.gilmore20@gmail.com>
2023-12-14 17:20:55 -0600

    When enter is pressed it will look for your input and then run a limit return on the api. Gathers the data and then run a form to gather info into database.

--- src/main/resources/static/js/searchForBookNavbar.js
+++ src/main/resources/static/js/searchForBookNavbar.js
@@ -1,6 +1,5 @@
 "use strict";
 (async () => {
-    // Why did I use async IIFE instead of function?
 
     //-- Search for Book Input
     const searchBookInput = document.getElementById("book-search-input");
@@ -13,22 +12,110 @@
     document.addEventListener("DOMContentLoaded", () =>{
         searchBookInput.value = "";
         searchBookCancelButton.style.display = 'none';
+        searchBookResultsContainer.style.display ="none";
     })
 
     //-- Everytime the cancel button is clicked in the input search it clears input and resets
     searchBookCancelButton.addEventListener("click", () => {
         searchBookInput.value = "";
         searchBookCancelButton.style.display = 'none';
+        searchBookResultsContainer.style.display ="none";
     })
 
     //-- Everytime there is an input show cancel button, when enter pushed then start the search
     searchBookInput.addEventListener("keydown", (e) =>{
+        //-- Show Cancel Button
         searchBookCancelButton.style.display ="block";
+
+        //-- When user press enter
         if (e.keyCode === 13){
             console.log("Enter Pressed")
+            showSearchBook()
         }
     })
 
+    //-- Notes:
+    //-- 'await' used to wait for promises to resolve before continuing with the execution of the code.
+    //-- Typically, you would expect to see asynchronous operations, such as fetching data from an API, inside an asynchronous function.
+
+    //-- Everytime called it fetches the api with book title input
+    const  gatherBooks = async (bookTitle) =>{
+        console.log("Gathering Information! for:"+bookTitle);
+        let googleAPIURL = `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&printType=books`
+        const gatheringData = await fetch(googleAPIURL);
+        const turningDataIntoJSON = await gatheringData.json();
+        return turningDataIntoJSON;
+    }
+
+
+    const showSearchBook = async () => {
+        if (searchBookInput.value !== "") {
+            //-- Whole search Book Container
+            searchBookResultsContainer.style.display = "flex";
+            //-- Data is going to call the function to get the books
+            let searchData = await gatherBooks(`${searchBookInput.value}&maxResults=10`);
+            //-- Pass data into dataCheck function, if correct display info
+            await dataCheck(searchData);
+
+        } else {
+            searchBookResultsContainer.style.display = "none";
+        }
+    }
+
+    const dataCheck = async (data) => {
+        console.log("Check for Error");
+        if (data.error) {
+            searchBookResultsContainer.innerHTML = `<div>Network Problem</div>`
+        } else if (data.totalItems === 0) {
+            searchBookResultsContainer.innerHTML = `<div class='prompt'>Try a different term</div>`;
+        } else if (data.totalItems === undefined) {
+            searchBookResultsContainer.innerHTML = `<div class='prompt'>Network problem!</div>`;
+        } else {
+            console.log("No Error Move ON");
+            searchBookResultsContainer.innerHTML = data.items.map( (({ volumeInfo }) => {
+                console.log(volumeInfo);
+                console.log("displaying all the search book container");
+                console.log(volumeInfo.title);
+                console.log(volumeInfo.industryIdentifiers[0].identifier);
+                console.log(volumeInfo.imageLinks.thumbnail);
+                console.log(volumeInfo.description);
+                console.log(volumeInfo.pageCount);
+                console.log(volumeInfo.publishedDate);
+                console.log(volumeInfo.infoLink);
+                //If rating is not entered then return 1
+                const ratingValue = parseFloat(volumeInfo.averageRating) || 1;
+                //-- We pick the first author
+                const bookAuthors = volumeInfo.authors[0] || "";
+                //-- Genre category cant be null
+                const genreValue =  volumeInfo.categories || "not placed";
+                const bookCover =  volumeInfo.imageLinks.thumbnail;
+
+                return `<div class="nav-bar-search-form-container">
+                            <form method="POST" action="/api/book">
+                               <input type="hidden" name="title" value="${volumeInfo.title}" />
+                               <input type="hidden" name="isbn" value="${volumeInfo.industryIdentifiers[0].identifier}" />
+                               <input type="hidden" name="author" value="${bookAuthors}" />
+                               <input type="hidden" name="cover_page" value="${bookCover}" />
+                               <input type="hidden" name="rating" value="${ratingValue}" />
+                               <input type="hidden" name="description" value="${volumeInfo.description}"/>
+                               <input type="hidden" name="genre" value="${genreValue}" />
+                               <input type="hidden" name="page_count" value="${volumeInfo.pageCount}" />
+                               <input type="hidden" name="date_published" value="${volumeInfo.publishedDate}" />
+                               <input type="hidden" name="google_play" value="${volumeInfo.infoLink}" />
+                               <button type="submit" class="btn">
+                                    <img class="search-book-results-book-cover" src="${bookCover}" th:alt="${volumeInfo.title}">
+                                    <div class="nav-search-book-info-container">
+                                        <h4 class="nav-search-book-result-title">
+                                            <div class="nav-search-book-results">${volumeInfo.title}</div>
+                                        </h4>
+                                        <p class="nav-search-bar-results-authors">${bookAuthors}</p>
+                                    </div>
+                               </button>
+                            </form>
+                    </div>`
+            }));
+        }
+    }
 
 
 
