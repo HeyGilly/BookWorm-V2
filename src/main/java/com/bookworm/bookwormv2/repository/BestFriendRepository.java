407e66c4f4fe3ebd1ce2a5578e96bc9ddec0c5ae
heygilly <nick.gilmore20@gmail.com>
2024-01-02 19:32:08 -0600

    Add a functional alert icon, when a user got a friend request that is pending.

--- src/main/java/com/bookworm/bookwormv2/repository/BestFriendRepository.java
+++ src/main/java/com/bookworm/bookwormv2/repository/BestFriendRepository.java
@@ -1,8 +1,13 @@
 package com.bookworm.bookwormv2.repository;
 
 import com.bookworm.bookwormv2.models.BestFriends;
+import com.bookworm.bookwormv2.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BestFriendRepository extends JpaRepository<BestFriends,Long> {
+    boolean existsByUserAndStatus(User user, String status);
+
 
 }
