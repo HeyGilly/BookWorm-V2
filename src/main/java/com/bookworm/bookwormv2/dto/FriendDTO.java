1e1e966bc872d8e1275dcb1e01aff2c80f7c31c2
heygilly <nick.gilmore20@gmail.com>
2024-01-07 19:46:16 -0600

    Styled Bestfriends on userprofile. Added a userId and image to DTO. From the Service, grabbed the img, and id and passed to DTO

--- src/main/java/com/bookworm/bookwormv2/dto/FriendDTO.java
+++ src/main/java/com/bookworm/bookwormv2/dto/FriendDTO.java
@@ -1,15 +1,27 @@
 package com.bookworm.bookwormv2.dto;
 
 public class FriendDTO {
+    private Long userId;
     private String friendUsername;
     private String status;
+    private String profilePicture;
 
     public FriendDTO() {
     }
 
-    public FriendDTO(String friendUsername, String status) {
+    public FriendDTO(Long userId, String friendUsername, String status, String profilePicture) {
+        this.userId = userId;
         this.friendUsername = friendUsername;
         this.status = status;
+        this.profilePicture = profilePicture;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     public String getFriendUsername() {
@@ -26,5 +38,13 @@ public class FriendDTO {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
     }
 }
--- src/main/java/com/bookworm/bookwormv2/service/BestFriendService.java
+++ src/main/java/com/bookworm/bookwormv2/service/BestFriendService.java
@@ -25,8 +25,12 @@ public class BestFriendService {
         for (BestFriends bff : bestFriends){
             //-- Searching for whether are you user or friend columns in the entity.
             String friendUsername = bff.getUser().getId().equals(userId) ? bff.getFriend().getUsername() : bff.getUser().getUsername();
+            //-- Whether are you user or friend for Profile Picture.
+            String profilePicture = bff.getUser().getId().equals(userId) ? bff.getFriend().getProfilePicturePath() : bff.getUser().getProfilePicturePath();
+            //-- Where User or friend for Id
+            Long someoneId = bff.getUser().getId().equals(userId) ? bff.getFriend().getId() : bff.getUser().getId();
             //-- Now passing the Username and status to the DTO
-            FriendDTO dto = new FriendDTO(friendUsername, bff.getStatus());
+            FriendDTO dto = new FriendDTO(someoneId, friendUsername, bff.getStatus(), profilePicture);
             //-- Results of DTO and now added.
             friendDTOs.add(dto);
         }
